d3af825afed1ad083522705374ea3199b8638e56
Oscar Okumu <ookumu@songa.app>
2023-05-20 23:25:46 +0300

    initial commit

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,21 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+     species = "Homo sapiens"
+    def __init__(self, name):
+        self.name = name
+        self._age = 0
+
+    def get_age(self):
+        print("Retrieving age.")
+        return self._age
+
+    def set_age(self, age):
+        if (type(age) in (int, float)) and (1 <= age <= 25):
+            print(f"Setting age to { age }.")
+            self._age = age
+
+        else:
+            print("Age must be a number between 0 and 120.")
+
+    age = property(get_age, set_age,)
